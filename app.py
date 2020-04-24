ab36af6f361e6022d79bed49c89c72b64384bea8
miachang <mia.chang@akelius.de>
2020-04-24 21:03:11 +0200

    step3 flask app

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask
+from flask import request
+from utils.prepare import load_model
+
+app = Flask(__name__)
+model = load_model("data/models/iris_model.pkl")
+
+
+@app.route("/")
+def hello_world():
+    return "Hello, World!"
+
+
+@app.route("/iris", methods=["GET"])
+def predict():
+
+    print(request.args)
+    sepal_length = float(request.args.get("sepal_length"))
+    sepal_width = float(request.args.get("sepal_width"))
+    petal_length = float(request.args.get("petal_length"))
+    petal_width = float(request.args.get("petal_width"))
+
+    pred = model.predict([[sepal_length, sepal_width, petal_length, petal_width]])
+
+    response = {
+        "input": [sepal_length, sepal_width, petal_length, petal_width],
+        "pred": pred.tolist(),
+    }
+    return response
